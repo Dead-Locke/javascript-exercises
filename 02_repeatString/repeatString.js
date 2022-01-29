e00e92ab5ff1537f9cf50efcc7b407c58f48bcc3
Dead-Locke <impulse25@ymail.com>
2022-01-28 22:49:52 -0500

    Solved the following excersises: - helloWorld.js - repeatString.js - reverseString.js - removeFromArray.js - sumAll.js  required a typeof check instead of isNaN()

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    var final = '';
+    for(let i = 0; i < num; i++){
+        final += string;
+    }
+    return num < 0? 'ERROR' : final;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if ((typeof a != "number" || typeof b != "number" )|| (a < 0 || b < 0)) return 'ERROR'; // ERROR for negative and NaN
+    
+    if(a > b){ // switch the numbers if the bigger number is the first argument
+        var temp;
+        temp = a; 
+        a = b;
+        b = temp;
+    }
 
+    return (a+b)*(b-a+1)/2 //
 };
 
 // Do not edit below this line
